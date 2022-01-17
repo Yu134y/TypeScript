0b1496f9cbdd34b4e790f4d4adfa133dbb8b199c
Yu134y <y.onosaka134@gmail.com>
2022-01-17 14:13:20 +0900

    add:object-types

--- ts_basic/src/index.ts
+++ ts_basic/src/index.ts
@@ -7,15 +7,22 @@
 // unknownSample()
 
 // 04.関数の型定義
-import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic'
-import { isUsersSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+// import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic'
+// import { isUsersSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
 
-logMessage('Hello TypeScript!')
-logMessage2('Hello TypeScript!')
-logMessage3('Hello TypeScript!')
-logMessage4('Hello TypeScript!')
-isUsersSignedIn('ABC', 'yu')
-isUsersSignedIn('DEF')
-isUserSignedIn2('ABC')
-const sum = sumProductsPrice(100, 200, 300, 400, 500)
-console.log('Function parameters sample 5:', sum)
+// logMessage('Hello TypeScript!')
+// logMessage2('Hello TypeScript!')
+// logMessage3('Hello TypeScript!')
+// logMessage4('Hello TypeScript!')
+// isUsersSignedIn('ABC', 'yu')
+// isUsersSignedIn('DEF')
+// isUserSignedIn2('ABC')
+// const sum = sumProductsPrice(100, 200, 300, 400, 500)
+// console.log('Function parameters sample 5:', sum)
+
+// 05.オブジェクトの型定義
+import objectSample from './object/object'
+import typeAliasSample from './object/alias'
+
+objectSample()
+typeAliasSample()
---
+++ ts_basic/src/object/object.ts
@@ -0,0 +1,51 @@
+export default function objectSample() {
+  const a: object = {
+    name: 'yu',
+    age: 22,
+  }
+  //   a.name  // aというobjectにはnameというプロパティがないとエラーが出る
+
+  // オブジェクトリテラル記法で型定義
+  let country: {
+    language: string
+    name: string
+  } = {
+    language: 'Japanese',
+    name: 'Japan',
+  }
+  console.log('Object object sample1:', country)
+
+  country = {
+    language: 'English',
+    name: 'United States of America',
+  }
+  console.log('Object object sample2:', country)
+
+  //   オプショナルとreadonly
+  const profile: {
+    age: number
+    lastName: string
+    readonly firstName: string
+    gender?: string
+  } = {
+    age: 22,
+    lastName: 'Yamada',
+    firstName: 'Taro',
+  }
+
+  profile.gender = 'male'
+  profile.lastName = 'Suzuki'
+  //   profile.firstName = 'Ichiro'  // firstNameプロパティはreadonlyなので再代入不可
+  console.log('Object object sample3:', profile)
+
+  //   インデックスシグネチャ
+  const capitals: {
+    [countryName: string]: string
+  } = {
+    Japan: 'Tokyo',
+    Korea: 'Seoul',
+  }
+  capitals.China = 'Beijing'
+  capitals.Canada = 'Ottawa'
+  console.log('Object object sample 4:', capitals)
+}
