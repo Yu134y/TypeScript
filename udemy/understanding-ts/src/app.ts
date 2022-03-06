7d755c40cea28d14f41393c39e6e28124176c4c5
Yu134y <y.onosaka134@gmail.com>
2022-03-06 14:47:24 +0900

    add:understanding ts file

--- udemy/understanding-ts/src/app.ts
+++ udemy/understanding-ts/src/app.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "AIzaSyCuRvP0M6G9L5yXIOEEC8WXpAY6XMSRCfs";
+const GOOGLE_API_KEY = "";
 
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
