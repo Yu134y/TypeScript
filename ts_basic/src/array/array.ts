cda3b52fe38cb5273018f8c1d778df7af64fe822
Yu134y <y.onosaka134@gmail.com>
2022-03-07 22:21:40 +0900

    add:array-types

--- ts_basic/src/array/array.ts
+++ ts_basic/src/array/array.ts
@@ -2,36 +2,47 @@ export default function arraySample() {
   // シンプルな配列の型定義
   const colors: string[] = ['red', 'blue']
   colors.push('yellow')
-  //   colors.push(123)  // 他の型は入れられない
+  // colors.push(123)  // 他の型は入れられない
   console.log('Array array sample 1:', colors)
 
+  // 型定義方法：T[]とArray<T>は同義
   const even: Array<number> = [2, 4, 6]
   even.push(8)
-  //   even.push('10')  // 他の型は入れられない
+  // even.push('10')  // 他の型は入れられない
   console.log('Array array sample 2:', even)
 
+  // 合併型も使える
   const ids: (string | number)[] = ['ABC', 123]
   ids.push('DEF')
   ids.push(456)
-  //   ids.push(true)  // 型定義していない型は入れられない
+  // ids.push(true)  // 型定義していない型は入れられない
   console.log('Array array sample 3:', ids)
 
-  //   配列の型推論
+  // 配列の型推論
   const generateSomeArray = () => {
     const _someArray = [] // any[]
-    _someArray.push(123) // number[]
-    _someArray.push('ABC') // (string | number)[]
+    _someArray.push(123) // number[]として推論される
+    _someArray.push('ABC') // (string | number)[]として推論される
     return _someArray
   }
 
   const someArray = generateSomeArray()
   someArray.push(456)
-  //   someArray.push(true)
+  // someArray.push(true)  // 型定義していない型は入れられない
   console.log('Array array sample 4:', someArray)
 
+  // JavaScriptの配列はconstで宣言してもミュータブル(書き換え可)
+  const mutableNumbers: number[] = [1, 2, 3]
+  mutableNumbers[2] = 4
+
   // イミュータブルな配列
+  // readonlyでイミュータブル(書き換え不可)な配列/タプルを作れる
   const commands: readonly string[] = ['git add', 'git commit', 'git push']
   // commands.push('git fetch')  // 追加不可
   // commands[2] = 'git pull'  // 代入不可
   console.log('Array array sample 5:', commands)
+
+  // 他の書き方
+  const numbers: ReadonlyArray<number> = [1, 2, 3]
+  const names: Readonly<string[]> = ['Taro', 'Yu']
 }
--- ts_basic/src/array/tuple.ts
+++ ts_basic/src/array/tuple.ts
@@ -1,12 +1,12 @@
 export default function tupleSample() {
   // 一般的なタプルの型定義
   let response: [number, string] = [200, 'OK']
-  //   response = [400, 'Bad Request', 'Email parameter is missing']  // 定義された要素数と合わない
-  //   response = ['400', 'Bad Request']  // 違う型は代入できない
+  // response = [400, 'Bad Request', 'Email parameter is missing']  // 定義された要素数と合わない
+  // response = ['400', 'Bad Request']  // 違う型は代入できない
   console.log('Array tuple sample 1:', response)
 
-  //   可変長引数を使ったタプル
-  const girlfriends: [string, ...string[]] = ['Mina', 'Mia', 'Miu']
+  // 可変長引数を使ったタプル
+  const girlfriends: [string, ...string[]] = ['Mina', 'Mio', 'Miu']
   girlfriends.push('Saki')
   console.log('Array tuple sample 2:', girlfriends)
 }
